86044e9568eab7b685d55378c2d250403a87e18f
camfeitosa <camilafeitosacoutinho@gmail.com>
2024-07-31 18:11:54 -0300

    projeto final

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,8 +1,9 @@
 export default function umCPF(campo) {
   const cpf = campo.value.replace(/\.|-/g, "")
-  validaNumerosRepetidos(cpf)
-  
-  console.log(validaNumerosRepetidos(cpf))
+
+  if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)){
+    campo.setCustomValidity('Esse CPF não é válido')
+  }
 }
 
 function validaNumerosRepetidos(cpf){
@@ -19,5 +20,42 @@ function validaNumerosRepetidos(cpf){
     '99999999999'
     ]
 
-    return numerosRepetidos.includes(cpf)
+    return numerosRepetidos.includes(cpf) // true = cpf inválido
 }
+
+function validaPrimeiroDigito(cpf){
+  let soma = 0
+  let multiplicador = 10
+
+  for(let tamanho = 0; tamanho < 9; tamanho++){
+    soma += cpf[tamanho] * multiplicador
+    multiplicador--
+  }
+
+  soma = (soma * 10) % 11
+
+  if(soma == 10 || soma == 11){
+    soma = 0
+  }
+
+  return soma != cpf[9] // true = cpf inválido
+}
+
+function validaSegundoDigito(cpf){
+  let soma = 0
+  let multiplicador = 11
+
+  for(let tamanho = 0; tamanho < 10; tamanho++){
+    soma += cpf[tamanho] * multiplicador
+    multiplicador--
+  }
+
+  soma = (soma * 10) % 11
+
+  if(soma == 10 || soma == 11){
+    soma = 0
+  }
+
+  return soma != cpf[10] // true = cpf inválido
+}
+
